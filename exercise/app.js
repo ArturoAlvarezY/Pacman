9e666de99c4b4e81d05140d6012a215d1687160d
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-02 12:14:52 +0200

    make eatable the yellow ghost!

---
+++ exercise/app.js
@@ -0,0 +1,7 @@
+const ghost= document.querySelector('.ghost');
+const pacman= document.querySelector('.pacmanImage')
+
+ghost.addEventListener("mouseover", ()=>{
+    ghost.src= '/img/pac-man-logo.png';
+})
+
