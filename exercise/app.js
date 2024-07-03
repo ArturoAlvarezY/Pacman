d3d7d41d11431969f2a66c55d9a53d117e094c39
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-03 23:25:46 +0200

    add animation to my ghost it moves in a square!

--- exercise/app.js
+++ exercise/app.js
@@ -1,64 +1,26 @@
 const ghost= document.getElementById('ghost');
 const pacman= document.querySelector('.pacmanImage');
 const score= document.getElementById('score');
-
 let pacmanRemoved = false; 
 
-ghost.addEventListener("mouseover", () => {
-  if (!pacmanRemoved) { 
-    ghost.src = pacman.src;
-    pacman.src = pacman.remove(); 
-    pacmanRemoved = true; 
-  }
-})
 
+const ghostAnimation= ghost.animate([
+    
+    { transform: 'translate(0, 0)' },
+    { transform: 'translate(1000px, 0)' },
+    { transform: 'translate(1000px, 1000px)' },
+    { transform: 'translate(0, 1000px)' },
+    { transform: 'translate(0, 0)' }
+  ], {
+    duration: 2000,
+    iterations: Infinity
+  });
 
-
-
-
-
-
-
-///Make a function to make the ghost appear each 2 seconds in different positions
-
-const ghostAppear = () =>
-{
-
-};
-
-setTimeout(2000, )
-
-
-///Make a if wich says that if pacman eat a phantom, score===100
-
-
-const myScore=()=>{
-
-}
-
-
-///Make another if wich says that if Pacman eat 3 phantoms or Score= 300, make an out of a cherry
-
-const outCherry = () =>
-{
-
-};
-
-
-
-
-///Make another if wich says that if Pacman eat a Cherry, it gives me 500 points!
-
-const eatCherry = () =>
-{
-
-};
-
-
-
-///Make the last if wich says that if Pacman get 5.000 score points, return GAME OVER
-
-const gameOver = () =>
-{
-
-};
+  const stopAnimation=  ghost.addEventListener("mouseover", () => {
+    if (pacmanRemoved === false) { 
+      ghost.src = pacman.src;
+      pacman.src = pacman.remove();   
+      pacmanRemoved= true;  
+      ghostAnimation.pause();
+      score.textContent += 100
+    }});
